f605307b0f2c3031b6ef76f110d702dee237ed35
Paulo Sérgio <pamorimmonico@gmail.com>
2026-02-14 08:23:04 -0300

    remove unused lsp config files (nvim-lspconfig handle)

--- lsp/systemd_ls.lua
+++
@@ -1,8 +0,0 @@
----@brief
----
---- Renamed to [systemd_lsp](#systemd_lsp)
-
-vim.deprecate('systemd_ls', 'systemd_lsp', '2.0.0', 'nvim-lspconfig', false)
-
----@type vim.lsp.Config
-return vim.lsp.config.systemd_lsp
--- lsp/vscoqtop.lua
+++
@@ -1,8 +0,0 @@
----@brief
----
---- Renamed to [vsrocq](#vsrocq)
-
-vim.deprecate('vscoqtop', 'vsrocq', '2.0.0', 'nvim-lspconfig', false)
-
----@type vim.lsp.Config
-return vim.lsp.config.vsrocq
