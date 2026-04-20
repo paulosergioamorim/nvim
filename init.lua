e6877716a8586afac36b00ded6c75b772cad9897
Paulo Sérgio <pamorimmonico@gmail.com>
2026-04-20 11:53:18 -0300

    some fixes

--- init.lua
+++ init.lua
@@ -16,50 +16,30 @@ vim.o.undofile = true
 vim.o.breakindent = true
 vim.o.confirm = true
 
+-- Format and Quickfix
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+-- Telescope LSP
+vim.keymap.set("n", "grd", "<cmd>Telescope lsp_definitions<CR>")
+vim.keymap.set("n", "gri", "<cmd>Telescope lsp_implementations<CR>")
+vim.keymap.set("n", "grr", "<cmd>Telescope lsp_references<CR>")
+-- Telescope
+vim.keymap.set("n", "<leader>sh", "<cmd>Telescope help_tags<CR>")
+vim.keymap.set("n", "<leader>sk", "<cmd>Telescope keymaps<CR>")
+vim.keymap.set("n", "<leader>sf", "<cmd>Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>si", "<cmd>Telescope git_files<CR>")
+vim.keymap.set("n", "<leader>ss", "<cmd>Telescope builtin<CR>")
+vim.keymap.set("n", "<leader>sw", "<cmd>Telescope grep_string<CR>")
+vim.keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>sd", "<cmd>Telescope diagnostics<CR>")
+vim.keymap.set("n", "<leader>sr", "<cmd>Telescope resume<CR>")
+vim.keymap.set("n", "<leader>s.", "<cmd>Telescope oldfiles<CR>")
+vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope buffers<CR>")
+-- Force hjkl
 vim.keymap.set("n", "<left>", "<cmd>echo \"Use h to move!!\"<CR>")
 vim.keymap.set("n", "<right>", "<cmd>echo \"Use l to move!!\"<CR>")
 vim.keymap.set("n", "<up>", "<cmd>echo \"Use k to move!!\"<CR>")
 vim.keymap.set("n", "<down>", "<cmd>echo \"Use j to move!!\"<CR>")
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("paulosergio-highlight-yank", { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("paulosergio-lsp-attach", {}),
-    callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-        if client:supports_method("textDocument/formatting") then
-            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-        end
-
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "grn", vim.lsp.buf.rename)
-        vim.keymap.set({ "n", "x" }, "gra", vim.lsp.buf.code_action)
-        vim.keymap.set("n", "grr", builtin.lsp_references)
-        vim.keymap.set("n", "gri", builtin.lsp_implementations)
-        vim.keymap.set("n", "grd", builtin.lsp_definitions)
-        vim.keymap.set("n", "grD", vim.lsp.buf.declaration)
-        vim.keymap.set("n", "gO", builtin.lsp_document_symbols)
-        vim.keymap.set("n", "gW", builtin.lsp_dynamic_workspace_symbols)
-        vim.keymap.set("n", "grt", builtin.lsp_type_definitions)
-    end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-    callback = function(args)
-        local lang = vim.treesitter.language.get_lang(args.match)
-        if lang and vim.treesitter.language.add(lang) then
-            vim.treesitter.start(args.buf)
-        end
-    end,
-})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) or vim.loop.fs_stat(lazypath) then
@@ -122,24 +102,13 @@ local plugins = {
     },
     {
         "nvim-telescope/telescope.nvim",
+        version = "*",
         dependencies = {
             "nvim-lua/plenary.nvim",
+            -- optional but recommended
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         },
-        config = function()
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>sh", builtin.help_tags)
-            vim.keymap.set("n", "<leader>sk", builtin.keymaps)
-            vim.keymap.set("n", "<leader>sf", builtin.find_files)
-            vim.keymap.set("n", "<leader>si", builtin.git_files)
-            vim.keymap.set("n", "<leader>ss", builtin.builtin)
-            vim.keymap.set("n", "<leader>sw", builtin.grep_string)
-            vim.keymap.set("n", "<leader>sg", builtin.live_grep)
-            vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
-            vim.keymap.set("n", "<leader>sr", builtin.resume)
-            vim.keymap.set("n", "<leader>s.", builtin.oldfiles)
-            vim.keymap.set("n", "<leader><leader>", builtin.buffers)
-        end
+        opts = {}
     },
     {
         "vague-theme/vague.nvim",
@@ -155,13 +124,35 @@ local plugins = {
 
 require("lazy").setup(plugins, opts)
 
+-- Colorscheme and UI2
 vim.cmd("colorscheme vague")
 vim.cmd("hi statusline guibg=NONE")
+require("vim._core.ui2").enable({})
+-- Fix Float Window Color
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "BlinkCmpMenu", { link = "Normal" })
+vim.api.nvim_set_hl(0, "BlinkCmpKind", { link = "Normal" })
+
 vim.cmd("packadd nvim.undotree")
+
+vim.lsp.enable({ "lua_ls", "clangd", "ts_ls", "pyright", "roslyn_ls", "jdtls", "gopls" })
 
 require("nvim-treesitter").install({ "c", "make", "bash", "html", "tsx", "typescript", "css", "python", "haskell", "xml",
     "markdown", "go" })
 
-vim.lsp.enable({ "lua_ls", "clangd", "ts_ls", "pyright", "roslyn_ls", "jdtls", "gopls" })
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("paulosergio-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
 
-require("vim._core.ui2").enable({})
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if lang and vim.treesitter.language.add(lang) then
+            vim.treesitter.start(args.buf)
+        end
+    end,
+})
